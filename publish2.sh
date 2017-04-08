82f338ef61381f3d110497913552dc8268c39a62
Eric Haas <ehaas@healthedatainc.com>
2017-04-07 19:07:37 -0700

    Update the structure definition template to section number based on the SD index

--- publish2.sh
+++ publish2.sh
@@ -8,7 +8,7 @@ echo "================================================================="
 sleep 1
 git status
 sleep 3
-python3 $name.py
+python3 definitions.py
 sleep 3
 git status
 java -jar ${path2}org.hl7.fhir.igpublisher.jar -ig ${path1}ig.json
